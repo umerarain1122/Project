8be78cc4a483a868575294b3704acf1ab9bb0dc9
Maleeka07 <maleekamuhammadamin@gmail.com>
2024-05-20 03:47:05 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 class Main{
   public static void main(String args[]){
-    System.out.println("Fifth Commit");
+    System.out.println("Everything is going fantastic in sindh university after i did friendship with Hassan and Umer");
   }
 }
