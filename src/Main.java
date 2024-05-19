00bd730ea899bc15c7511134f06cbff2382402c6
hassan223190 <hassansangi00@gmail.com>
2024-05-20 04:26:30 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 class Main{
   public static void main(String args[]){
-    System.out.println("Our leader umer arain always do hard work.");
+    System.out.println("credit of this project goes to our honourable leader umer arain.");
   }
 }
